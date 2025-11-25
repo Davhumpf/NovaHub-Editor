e3261dc9b37985d2c4162eba25f4d8e1cd453582
Davhumpf <scpu.v1@gmail.com>
2025-11-24 21:35:43 -0500

    Hide vertical scrollbars in panels

--- src/hooks/useExtensions.ts
+++
@@ -1,6 +0,0 @@
-// hooks/useExtensions.ts
-import { useState, useCallback, useEffect } from 'react';
-import { Extension } from '@/components/panels/ExtensionsPanel';
-import { ExtensionManager } from '@/lib/extensionManager';
-
-const
