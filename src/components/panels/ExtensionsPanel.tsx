f0f2aff65e13c67a663ea4c8d77c0919c6088ea7
Davhumpf <scpu.v1@gmail.com>
2025-11-25 17:45:07 -0500

    Fix JSX syntax error: Replace Tailwind arbitrary class with inline style
    Updated max-height style to inline style for better control.

--- src/components/panels/ExtensionsPanel.tsx
+++ src/components/panels/ExtensionsPanel.tsx
@@ -309,8 +309,7 @@ export default function ExtensionsPanel({ theme: legacyTheme = 'dark' }: Extensi
       </div>
 
       {/* Extensions List - FIXED WITH SCROLL */}
-      <div className="flex-1 overflow-y-auto p-3 space-y-2 scrollbar-hide" max-h-[calc(100vh-250px)]>
-        {filteredExtensions.map(ext => (
+      <div className="flex-1 overflow-y-auto p-3 space-y-2 scrollbar-hide" style={{maxHeight: 'calc(100vh - 250px)'}}       {filteredExtensions.map(ext =>
           <div
             key={ext.id}
             className="p-3 rounded border"
